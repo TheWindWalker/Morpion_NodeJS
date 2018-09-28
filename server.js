3f24bec9b436fd7cc8b5d105975375b1982a2191
TheWindWalker <arnaud.borio@orange.fr>
2018-09-28 11:33:34 +0200

    Interface

--- server.js
+++ server.js
@@ -1,63 +1,30 @@
+//------------------------------------------------------------------------------
 var express = require('express');
 var serve_static = require('serve-static');
 var http = require('http');
-
-var users = [];
-
-
-function checkuser(pseudo){
-    for(var i=0; i<users.length; i++){
-        if(users[i]==pseudo){return true;}
-    }
-    return false;
-}
-
-function add_user(socket,pseudo){
-    var User_present= checkuser(pseudo);
-    if(User_present== false){
-        var envoie=pseudo;
-        //var envoie.id=socket.id;
-        users.push(envoie);
-        //socket.emit('connect_success',envoie.pseudo);
-        console.log(envoie+" c'est connecté avec l'id: ");
-    }
-    else{
-        console.log("User already connected");
-        //var message={code:'CONNECT_FAILED', reason:"Pseudo utilisé, veuillez en choisir un autre!"};
-        //socket.emit('code',message);
-    }
-}
-
-/*function private_message(socket,from,to,msg)
-{
-socket.on('say to someone', (id, msg) => {
-// send a private message to the socket with the given id
-socket.to(id).emit('my message', msg);
-});
-}*/
-
-
-
- var app = express();
- //Activation du serveur statique
- app.use(serve_static(__dirname+"/public"));
- //Récupération du serveur http de l'application
- var serveur = http.Server(app);
-
+var app = express();
+//Activation du serveur statique
+app.use(serve_static(__dirname+"/public"));
+//Récupération du serveur http de l'application
+var serveur = http.Server(app);
+//------------------------------------------------------------------------------
  //Ecoute sur un seul port
- serveur.listen(8080, function()
- {
- console.log("Serveur en écoute sur le port 8080");
- });
-
- //Gestion du temps réel
- var io = require('socket.io').listen(serveur);//Import du module pour le serveur
-
+serveur.listen(8080, function()
+{
+console.log("Serveur en écoute sur le port 8080");
+});
+//Gestion du temps réel
+var io = require('socket.io').listen(serveur);//Import du module pour le serveur
+//------------------------------------------------------------------------------
+var rooms = 0; //Room to play
+//------------------------------------------------------------------------------
+//Serveur
  io.sockets.on('connect', function (socket) {
-     console.log("Connect");
+
      socket.on('login', function(pseudo)
      {
-         add_user(socket,pseudo);
+         console.log("A user connect to the server!");
+
      });
 
      socket.on('disconnect', function(){
@@ -66,11 +33,43 @@ socket.to(id).emit('my message', msg);
 
 
 
-     socket.on('message', function(message)
-     {
-         console.log("J'ai reçu : "+message);
-         socket.emit('reponse', message);
-         socket.broadcast.emit('message', "J'ai reçu un message d'un utilisateur: "+message);
-     });
+     /**
+     * Create a new game room and notify the creator of game.
+     */
+    socket.on('createGame', function(data){
+      socket.join('room-' + ++rooms);
+      socket.emit('newGame', {name: data.name, room: 'room-'+rooms});
+    });
 
- });
+    /**
+     * Connect the Player 2 to the room he requested. Show error if room full.
+     */
+    socket.on('joinGame', function(data){
+      var room = io.nsps['/'].adapter.rooms[data.room];
+      if( room && room.length == 1){
+        socket.join(data.room);
+        socket.broadcast.to(data.room).emit('player1', {});
+        socket.emit('player2', {name: data.name, room: data.room })
+      }
+      else {
+        socket.emit('err', {message: 'Sorry, The room is full!'});
+      }
+    });
+
+    /**
+     * Handle the turn played by either player and notify the other.
+     */
+    socket.on('playTurn', function(data){
+      socket.broadcast.to(data.room).emit('turnPlayed', {
+        tile: data.tile,
+        room: data.room
+      });
+    });
+
+    /**
+     * Notify the players about the victor.
+     */
+    socket.on('gameEnded', function(data){
+      socket.broadcast.to(data.room).emit('gameEnd', data);
+    });
+});
